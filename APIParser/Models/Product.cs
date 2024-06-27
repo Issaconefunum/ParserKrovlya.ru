e69b114b48d65d8246439c8d795f8da30d89057e
Issaconefun <danyabeltyukov@yandex.ru>
2024-06-27 12:17:13 +0400

    Start project

---
+++ APIParser/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace APIParser.Models
+{
+    public class Product
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Price { get; set; } = string.Empty;
+
+
+    }
+}
---
+++ APIParser/Models/Request/ProductsRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIParser.Models.Request
+{
+    public class ProductsRequest
+    {
+        [Required(ErrorMessage="The field can not be empty")]
+        [MinLength(3, ErrorMessage = "Min length must be bigger than 3")]
+        [MaxLength(255, ErrorMessage = "Min length must be smaller than 255")]
+        public string SearchPhrase { get; set; } = string.Empty;
+
+     }
+}
